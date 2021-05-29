c688c193960941ef558f2cdc2f9d2f34a4579e6d
fabianasandoval <fabiana.sandoval82@gmail.com>
2021-05-29 18:55:22 -0500

    oka

---
+++ sesion 17/js/agenda1.js
@@ -0,0 +1,15 @@
+function guardarDatos(){
+    localStorage.nombre = document.getElementById("nombre").value;
+    localStorage.movil = document.getElementById("movil").value;
+}
+
+function recuperarDatos(){
+
+    if((localStorage.nombre != undefined) && (localStorage.movil != undefined)){
+        document.getElementById("datos").innerHTML = "Nombre: " + localStorage.nombre + "<br> Numero  de celular: " + localStorage.movil;
+    } else{
+        document.getElementById("datos").innerHTML = "No existen datos registrados";
+    }
+
+    
+}
