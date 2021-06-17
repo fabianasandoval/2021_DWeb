ecf3330f375bef0e5b6db710ef97c037faadf5f9
fabianasandoval <fabiana.sandoval82@gmail.com>
2021-06-17 12:49:18 -0500

    hola

--- sesion 17/js/agenda1.js
+++ sesion 17/js/agenda1.js
@@ -1,14 +1,11 @@
-function guardarDatos(){
+function guardarDatos() {
     localStorage.nombre = document.getElementById("nombre").value;
     localStorage.movil = document.getElementById("movil").value;
 }
-
-function recuperarDatos(){
-
-    if((localStorage.nombre != undefined) && (localStorage.movil != undefined)){
-        document.getElementById("datos").innerHTML = "Nombre: " + localStorage.nombre + "<br> Numero  de celular: " + localStorage.movil;
-    } else{
+function recuperarDatos() {
+    if ( (localStorage.nombre != undefined) && (localStorage.movil != undefined) ) {
+        document.getElementById("datos").innerHTML = "Nombre: " + localStorage.nombre + "<br> Num Celular: " + localStorage.movil;
+    } else {
         document.getElementById("datos").innerHTML = "No existen datos registrados";
     }
-
 }
